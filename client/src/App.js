602a583250971f81e86c80f4bad6d614dcd90a0b
wanghz_crm_cmi <wanghz_crm_cmi@si-tech.com.cn>
2023-08-16 17:15:50 +0800

    init

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- service/app.js
+++ service/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
+
+//允许跨域
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); //项目上线后改成页面的地址
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Content-Length,Authorization,Accept');
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  // res.header('Access-Control-Allow-Credentials', true);
+  res.header('X-Powered-By', ' 3.2.1');
+  if (req.method == 'OPTIONS') res.send(200); //让options请求快速返回
+  else next();
+});
+
 
 // 处理 GET 请求
 app.get('/', (req, res) => {
